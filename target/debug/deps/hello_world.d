16a0c2912279024af4b9eb0d0de40332182a3ab1
lucialacourtna <78623423+lucialacourtna@users.noreply.github.com>
2026-02-04 14:04:41 -0500

    Delete target directory

--- target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lucia\Documents\school\ywcc\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-C:\Users\lucia\Documents\school\ywcc\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-src\main.rs:
--- target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\lucia\Documents\school\ywcc\hello_world\target\debug\hello_world.exe: C:\Users\lucia\Documents\school\ywcc\hello_world\src\main.rs
